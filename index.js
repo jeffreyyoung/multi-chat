53a8b452a649cc2a315237a91c286a8ae72213b7
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-09-02 15:13:46 -0600

    shows user name on message send

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ io.on('connection', function(socket) {
 	});
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		io.emit('chat message', people[socket.id], msg);
 	})
 });
 
