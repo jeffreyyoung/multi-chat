63bc0297ad188024531b8e66a7c90fc5e414450a
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-09-12 01:06:41 -0600

    making port argument

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ hbs.registerHelper( 'eachInMap', function ( map, block ) {
 
 app.use('/public', express.static('public'));
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+	var port = process.env.PORT || 3000;
+	console.log('listening on *:' + port);
 });
 
